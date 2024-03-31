0d14e22d2ff9cc51a4206694def26d8a970df23a
oielbanna <>
2024-03-31 19:08:01 -0400

    animate landing

--- pages/landing.js
+++ pages/landing.js
@@ -1,19 +1,59 @@
 import styles from "@components/landing.module.css";
 import Link from 'next/link';
+import { motion } from "framer-motion";
 
 export default function Landing() {
+    const container = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.3
+            }
+        }
+    }
+    const itemMovement = 35;
+    const leftItem = {
+        hidden: { x: itemMovement, opacity: 0 },
+        show: { x: 0, opacity: 1 }
+    }
+    const rightItem = {
+        hidden: { x: itemMovement * -1, opacity: 0 },
+        show: { x: 0, opacity: 1 }
+    }
+
     return (
         <div className={styles.landing}>
-            <ul className={styles.list}>
-                <li><Link href="#">Animation Portfolio</Link></li>
-                <li><Link href="#">Music Videos</Link></li>
-                <li><Link href="#">Animated Illustrations</Link></li>
-            </ul>
+            <motion.ul
+                variants={container}
+                initial="hidden"
+                animate="show"
+                className={styles.list}>
+                <motion.li
+                    variants={leftItem}
+                ><Link href="#">Animation Portfolio</Link></motion.li>
+                <motion.li
+                    variants={leftItem}
+                ><Link href="#">Music Videos</Link></motion.li>
+                <motion.li
+                    variants={leftItem}
+                ><Link href="#">Animated Illustrations</Link></motion.li>
+            </motion.ul>
     <img src="https://static.wixstatic.com/media/b42ec7_66733353ca35499291579824de98eee1~mv2.png/v1/fill/w_477,h_683,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Kapal%C4%B1goz.png"></img>
-            <ul style={{ textAlign: 'right' }} className={styles.list}>
-                <li><Link href="#">Vertical Videos</Link></li>
-                <li><Link href="#">Photography</Link></li>
-                <li><Link href="#">About</Link></li>
-            </ul>
+            <motion.ul
+                variants={container}
+                initial="hidden"
+                animate="show"
+                style={{ textAlign: 'right' }} className={styles.list}>
+                <motion.li
+                    variants={rightItem}
+                ><Link href="#">Vertical Videos</Link></motion.li>
+                <motion.li
+                    variants={rightItem}
+                ><Link href="#">Photography</Link></motion.li>
+                <motion.li
+                    variants={rightItem}
+                ><Link href="#">About</Link></motion.li>
+            </motion.ul>
             </div>)
 }
