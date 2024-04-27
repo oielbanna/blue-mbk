7575b67d3b25039103a89afa34b5dccf73211257
oielbanna <>
2024-04-27 16:18:06 -0400

    use reduced vertical videos

--- components/vertical-videos.js
+++ components/vertical-videos.js
@@ -1,63 +1,50 @@
 export const VERTICAL_VIDEOS = [
   {
-    name: 'Alex-Canvas-02_4.mp4'
+    name: 'Alex.mp4'
   },
   {
-    name: 'Andreas-canvas01-02.mp4'
+    name: 'Anxiety.mp4'
   },
   {
-    name: 'Anxiety-Canvas.mp4'
+    name: 'Austin.mp4'
   },
   {
-    name: 'Austin Euphoria fotosuz dans.mp4'
+    name: 'Denny.mp4'
   },
   {
-    name: 'Denny-CANVAS-01_02.mp4'
+    name: 'Elizabeth.mp4'
   },
   {
-    name: 'elizabeth-canvas1.mp4'
+    name: 'GFTCS.mp4'
   },
   {
-    name: 'GFTCS-Canvas-01.mp4'
+    name: 'HL.mp4'
   },
   {
-    name: 'HL-canvas01.mp4'
+    name: 'JanE.mp4'
   },
   {
-    name: 'JanE-canvas-01.mp4'
+    name: 'JM.mp4'
   },
   {
-    name: 'Kenny-Canvas-02.mp4'
+    name: 'Josh.mp4'
   },
   {
-    name: 'JM-canvas-01revised.mp4'
+    name: 'Lost.mp4'
   },
   {
-    name: 'Josh_Canvas_01.mp4'
+    name: 'PC.mp4'
   },
   {
-    name: 'Kolby-FriendLikeYou-Canvas-01.mp4'
-  },
-
-  {
-    name: 'Kwanza-Problem-cavnas-revised02.mp4'
+    name: 'Rainer.mp4'
   },
   {
-    name: 'Lost-canvas-01.mp4'
+    name: 'TeeBallPaul.mp4'
   },
   {
-    name: 'PC_02.mp4'
+    name: 'TeeBallPaul-02.mp4'
   },
   {
-    name: 'Rainer-canvas01.mp4'
-  },
-  {
-    name: 'TeeBallPaul-Canvas-01.mp4'
-  },
-  {
-    name: 'TeeBallPaul-Canvas-02_1.mp4'
-  },
-  {
-    name: 'Will-Canvas-01.mp4'
+    name: 'Will.mp4'
   }
 ];
